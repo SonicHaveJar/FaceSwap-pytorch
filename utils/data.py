1e61c678f202afa6ec175129d6b240d16a4d3920
SonicHaveJar <javier.sancho.olano@gmail.com>
2019-11-24 21:01:45 +0100

    Fixing errors

--- utils/data.py
+++ utils/data.py
@@ -1,4 +1,8 @@
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
+import os
 
 #https://stackoverflow.com/a/50802257
 class LimitDataset(Dataset):
@@ -11,3 +15,25 @@ class LimitDataset(Dataset):
 
     def __getitem__(self, i):
         return self.dataset[i]
+
+# Default load function to make things easier #TODO: ADD VAL AND TEST SETS
+def autoloader(pathA, pathB, data_transforms=None, bs=32, num_workers=2, number_of_images=None):
+    if data_transforms is None:
+        data_transforms = transforms.Compose([
+            transforms.Resize(224),#RandomResizedCrop(224)??
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    
+    A_dataset = ImageFolder(os.path.join(pathA), data_transforms)
+    B_dataset = ImageFolder(os.path.join(pathB), data_transforms)
+
+    if not number_of_images is None:
+        A_dataset = LimitDataset(A_dataset, number_of_images)
+        B_dataset = LimitDataset(B_dataset, number_of_images)
+    
+    A_loader = DataLoader(A_dataset, batch_size=bs, shuffle=True, num_workers=num_workers)
+    B_loader = DataLoader(B_dataset, batch_size=bs, shuffle=True, num_workers=num_workers)
+
+    return A_loader, B_loader
--- utils/utils.py
+++ utils/utils.py
@@ -3,3 +3,13 @@ def compare(arg1, arg2, opt):
         return arg1 if arg1 < arg2 else arg2
     elif opt == 'bigger':
         return arg1 if arg1 > arg2 else arg2
+
+def forward(x, A, B, direction='A2B'):
+    if direction == 'A2B':
+        out = A.encoder(x)
+        out = B.decoder(out)
+        return out
+    else:        
+        out = B.encoder(x)
+        out = A.decoder(out)
+        return out
